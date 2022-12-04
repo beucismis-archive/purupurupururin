7e68da1758005dcd04d087ceb0b349965014cd2c
beucismis <beucismis@tutamail.com>
2022-12-04 19:52:43 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ OWM_ENDPOINT = "api.openweathermap.org/data/2.5/weather"
 http = PoolManager()
 
 
-def get_weather_id(lat: str, lon: str, appid: str) -> int:
+def get_weather_id(appid: str, lat: str, lon: str) -> int:
     response = http.request(
         "GET",
         OWM_ENDPOINT,
@@ -20,21 +20,21 @@ def get_weather_id(lat: str, lon: str, appid: str) -> int:
     return data["weather"][0]["id"]
 
 
-def send_notification(title: str, message: str) -> None:
+def send_notification(title: str, message: str, priority: str) -> None:
     for topic in NTFY_TOPICS:
         http.request(
             "POST",
             NTFY_ENDPOINT.format(topic),
             body=message,
-            headers={"Title": title, "Priority": NTFY_PRIORITY},
+            headers={"Title": title, "Priority": priority},
         )
 
 
 if __name__ == "__main__":
     start_time = time.time()
 
-    weather_id = get_weather_id(OWM_LAT, OWM_LON, OWM_APPID)
+    weather_id = get_weather_id(OWM_APPID, OWM_LAT, OWM_LON)
     message = SUGGESTIONS.get(weather_id).encode("utf-8")
-    send_notification(title=NTFY_TITLE, message=message)
+    send_notification(NTFY_TITLE, message, NTFY_PRIORITY)
 
     print("Finished in {} seconds".format(round(time.time() - start_time, 1)))
