8047cb48e9b3c98dd148a3e461245a3bd8326ca9
beucismis <beucismis@tutamail.com>
2025-04-06 17:20:51 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,51 +1,60 @@
-import time, json
-from urllib3 import PoolManager
+import time
+import json
+
+import requests
 
 from constant import *
 
 
-NTFY_ENDPOINT = "https://ntfy.sh/{}"
-OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
-OWM_ICON_PATH = "https://openweathermap.org/img/wn/{}@2x.png"
-http = PoolManager()
-
-
-def get_current_weather(appid: str, lat: str, lon: str) -> int:
-    response = http.request(
-        "GET",
-        OWM_ENDPOINT,
-        fields={"lat": lat, "lon": lon, "appid": appid, "units": "metric"},
+def get_current_weather(api_key: str, latitude: str, longitude: str, units: str) -> dict:
+    response = requests.get(
+        OWM_API_ENDPOINT,
+        params={"appid": api_key, "lat": latitude, "lon": longitude, "units": units},
     )
 
-    return json.loads(response.data.decode("utf-8"))
+    return response.json()
 
 
-def send_notification(title: str, message: str, icon: str, priority: str) -> None:
-    for topic in NTFY_TOPICS:
-        http.request(
-            "POST",
-            NTFY_ENDPOINT.format(topic),
-            body=message,
-            headers={"Title": title, "Icon": icon, "Priority": priority},
-        )
+def send_notification(
+    topic: str, title: str, message: str, icon: str, priority: int
+) -> None:
+    requests.post(
+        NTFY_API_ENDPOINT,
+        data=json.dumps(
+            {
+                "topic": topic,
+                "title": title,
+                "message": message,
+                "icon": icon,
+                "priority": priority,
+            }
+        ),
+    )
 
 
 def format_message(data: dict) -> str:
     temp = data["main"]["temp"]
     feels_like = data["main"]["feels_like"]
     humidity = data["main"]["humidity"]
-    suggestion = SUGGESTIONS.get(data["weather"][0]["id"])
+    suggestion = WEATHER_SUGGESTIONS.get(data["weather"][0]["id"])
 
-    return NTFY_MESSAGE_TEMPLATE.format(
-        suggestion, temp, feels_like, humidity,
-    ).encode("utf-8")
+    return NOTIFICATION_MESSAGE_TEMPLATE.format(
+        suggestion,
+        temp,
+        feels_like,
+        humidity,
+    )
 
 
 if __name__ == "__main__":
     start_time = time.time()
 
-    data = get_current_weather(OWM_APPID, OWM_LAT, OWM_LON)
-    icon = OWM_ICON_PATH.format(data["weather"][0]["icon"])
-    send_notification(NTFY_TITLE, format_message(data), icon, NTFY_PRIORITY)
+    current_weather = get_current_weather(OWM_API_KEY, LATITUDE, LONGITUDE, OWM_UNITS)
+    weather_icon = OWM_ICON_ENDPOINT.format(current_weather["weather"][0]["icon"])
+    message = format_message(current_weather)
+
+    send_notification(
+        NTFY_TOPIC, NOTIFICATION_TITLE, message, weather_icon, NTFY_PRIORITY
+    )
 
     print("Finished in {} seconds".format(round(time.time() - start_time, 1)))
